7d781c78142462392578e98d874f883deddb1a7f
KagurazakaMiyabi (神楽坂雅詩) <miyabi@uuu.moe>
2025-04-03 20:25:43 +0800

    创建项目

---
+++ NyarukoAppRelay/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NyarukoAppRelay
+{
+    /// <summary>
+    /// 程式主要入口類別
+    /// </summary>
+    static class Program
+    {
+        /// <summary>
+        /// 應用程式的主要進入點
+        /// </summary>
+        /// <param name="args">命令列參數</param>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // 啟用應用程式視覺樣式與文字渲染設定
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run();
+        }
+    }
+}
---
+++ NyarukoAppRelay/RelayContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+/// <summary>
+/// 處理系統匣圖示與處理序監控的應用程式上下文
+/// </summary>
+public class RelayContext : ApplicationContext
+{
+}
