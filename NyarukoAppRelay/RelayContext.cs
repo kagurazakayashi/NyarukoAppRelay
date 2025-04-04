76ecd59c848bc2420c768ebe82949a5a7c583c9c
KagurazakaMiyabi (神楽坂雅詩) <miyabi@uuu.moe>
2025-04-04 20:18:14 +0800

    传递输入参数和显示帮助

--- NyarukoAppRelay/RelayContext.cs
+++ NyarukoAppRelay/RelayContext.cs
@@ -10,4 +10,16 @@ using System.Windows.Forms;
 /// </summary>
 public class RelayContext : ApplicationContext
 {
+    private string _cmdA, _cmdE, _customTitle;
+
+    /// <summary>
+    /// 初始化監控上下文
+    /// </summary>
+    public RelayContext(string cmdA, string cmdE, string iconPath, string title)
+    {
+        _cmdA = cmdA;
+        _cmdE = cmdE;
+        _customTitle = title;
+        _startTime = DateTime.Now;
+    }
 }
